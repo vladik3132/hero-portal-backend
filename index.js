e6184ec8135042cfb2f184b956dc8d5cd93986fe
vladik3132 <wvladowv@gmail.com>
2025-11-24 22:23:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+// ===== FIREBASE INIT USING SECRET FILE =====
+const admin = require("firebase-admin");
+const serviceAccount = require("/etc/secrets/firebase-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Firestore instance
+const db = admin.firestore();
+module.exports.db = db;
+
+// ========== EXPRESS ==========
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,6 +23,7 @@ app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
 
+// ===== ROUTES =====
 app.use("/api/news", require("./routes/newsRoutes"));
 app.use("/api/gallery", require("./routes/galleryRoutes"));
 app.use("/api/heroes", require("./routes/heroesRoutes"));
